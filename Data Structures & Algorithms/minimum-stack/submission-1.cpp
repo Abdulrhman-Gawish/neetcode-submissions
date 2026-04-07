d5d34aaaadbe11003ca181f312dafc2821b2a086
Abdulrhman Gawish <152007715+Abdulrhman-Gawish@users.noreply.github.com>
2026-04-08 00:00:10 +0200

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,29 @@
+class MinStack {
+public:
+    stack<long long> st;
+    stack<long long> minSt;
+
+    MinStack() {}
+
+    void push(long long val) {
+        st.push(val);
+        if (minSt.empty() || val <= minSt.top()) {
+            minSt.push(val);
+        }
+    }
+
+    void pop() {
+        if (st.top() == minSt.top()) {
+            minSt.pop();
+        }
+        st.pop();
+    }
+
+    int top() {
+        return st.top();
+    }
+
+    int getMin() {
+        return minSt.top();
+    }
+};
