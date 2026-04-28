78dcfdac2d75f1c477b435ea7532943b107d22ac
Abdulrhman Gawish <152007715+Abdulrhman-Gawish@users.noreply.github.com>
2026-04-29 02:51:50 +0300

    Time: 0 ms (100.00%) | Memory: 8.9 MB (82.34%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
+        // first get freq for important chars;
         map<char, int> freq;
         for (char& c : text) {
-            if (c == 'b' || c == 'a' || c == 'l' || c == 'o' || c == 'n') {
+            if (c == 'b' || c == 'a' || c == 'l' || c == 'o' || c == 'n')
                 freq[c]++;
-            }
         }
-        string required = "balloon";
-        for (char c : required) {
-            if (freq[c] == 0)
+        // check exsit or not
+        vector<char> vec = {'b', 'a', 'l', 'o', 'n'};
+        for (char c : vec) {
+            if (freq[c] == 0) {
                 return 0;
+            }
         }
 
         int ans = INT_MAX;
-        for (auto itr : freq) {
-            if (itr.first == 'o' || itr.first == 'l') {
-                ans = min(ans, itr.second / 2);
-            } else {
-                ans = min(ans, itr.second);
-            }
-        }
+        ans = min(ans, freq['b']);
+        ans = min(ans, freq['a']);
+        ans = min(ans, freq['l']/2);
+        ans = min(ans, freq['o']/2);
+        ans = min(ans, freq['n']);
         return ans;
     }
 };
