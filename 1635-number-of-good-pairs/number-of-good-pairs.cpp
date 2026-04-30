ef3e96c056e5a5065abe9fd488e884ed1983001e
Abdulrhman Gawish <152007715+Abdulrhman-Gawish@users.noreply.github.com>
2026-04-30 23:29:56 +0300

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.81%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
         int n = nums.size(), ans = 0;
+        map<int, int> freq;
         for (int i = 0; i < n; i++) {
-            for (int k = i + 1; k < n; ++k) {
-                if (nums[i] == nums[k]){
-                    ans++;
-                }
-            }
+            freq[nums[i]]++;
+        }
+        for (const auto& i : freq) {
+            ans += i.second * (i.second - 1) / 2;
         }
         return ans;
     }
