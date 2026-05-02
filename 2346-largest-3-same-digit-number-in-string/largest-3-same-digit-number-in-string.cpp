3691abcaac80be9caeb682ce47a691ee3d706cfc
Abdulrhman Gawish <152007715+Abdulrhman-Gawish@users.noreply.github.com>
2026-05-02 21:07:22 +0300

    Time: 0 ms (100.00%) | Memory: 9.3 MB (9.96%) - LeetSync

---
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+      vector<string> vec = {"999", "888", "777", "666", "555", "444", "333", "222", "111", "000"};
+        for(string &str: vec){
+            if(num.contains(str)){
+                return str;
+            }
+        }
+        return "";
+    }
+};
