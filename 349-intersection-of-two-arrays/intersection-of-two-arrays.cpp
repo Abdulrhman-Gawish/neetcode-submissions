f45de597e080649221d9c635e5093255b2b1ac70
Abdulrhman Gawish <152007715+Abdulrhman-Gawish@users.noreply.github.com>
2026-05-06 22:27:20 +0300

    Time: 11 ms (5.04%) | Memory: 13.9 MB (81.36%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        bool existsInNums1[1005] = {false};
-        bool added[1005] = {false}; 
         vector<int> ans;
-
-        for (int num : nums1) {
-            existsInNums1[num] = true;
-        }
-
-        for (int num : nums2) {
-            if (existsInNums1[num] && !added[num]) {
-                ans.push_back(num);
-                added[num] = true;
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); ++j) {
+                if (nums1[i] == nums2[j]) {
+                    ans.push_back(nums1[i]);
+                }
             }
         }
 
+        sort(ans.begin(), ans.end());
+        auto it = unique(ans.begin(), ans.end());
+        ans.erase(it, ans.end());
         return ans;
-
     }
 };
