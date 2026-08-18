c2c2246d17b184403016dbd8794b7d06bc49b5a9
Abdulrhman Gawish <152007715+Abdulrhman-Gawish@users.noreply.github.com>
2026-08-18 14:19:06 +0300

    Time: 0 ms (100.00%) | Memory: 14 MB (84.18%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,15 +2,22 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         int n = prices.size();
+        stack<int> st;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (prices[i] >= prices[j]) {
-                    prices[i] -= prices[j];
-                    break;
-                }
+        for (int i = n - 1; i >= 0; i--) {
+            int original = prices[i];
+
+            while (!st.empty() && st.top() > original) {
+                st.pop();
             }
+
+            if (!st.empty()) {
+                prices[i] = original - st.top();
+            }
+
+            st.push(original);
         }
+
         return prices;
     }
 };
